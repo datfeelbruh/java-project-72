0fd22f2d1bc22d8f2ed2081d6ee660d3bf14e215
datfeelbruh <datfeelman@gmail.com>
2022-11-16 20:22:00 +0300

    first step

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,44 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.core.JavalinConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "3000");
+        LOGGER.info("port = " + port);
+        return Integer.valueOf(port);
+    }
+
+    private static String getMode() {
+        return System.getenv().getOrDefault("APP_ENV", "development");
+    }
+
+    private static boolean isProduction() {
+        return getMode().equals("production");
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.result("Hello world!"));
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            if (!isProduction()) {
+                config.enableDevLogging();
+            }
+        });
+
+        addRoutes(app);
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+}
